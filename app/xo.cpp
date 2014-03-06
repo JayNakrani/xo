efc7aa2ab9cd2df2d690930b51bc9ca18e9b48f1
Dhananjay Nakrani <dhananjaynakrani@gmail.com>
2014-03-06 11:56:36 +0530

    Improve Coding style.

--- app/xo.cpp
+++ app/xo.cpp
@@ -2,37 +2,55 @@
 
 using namespace std;
 
-int main()
+int main(void)
 {
-	int row=0,coulmn=0;
-	char winner='\0';
-	char userChoice='\0';
+	int row = 0,coulmn = 0;
+	char winner = '\0';
+	char userChoice = '\0';
 	grid pg;
-	cout<<"\nPlayer_1:-->\"X\"\nPlayer_2:-->\"O\"\n";
-    do
-    {
-          do
-            {
-               pg.display();
-		       cout<<"\nPlayer-1:";
-		       cin>>row>>coulmn;
-		       pg.take_input(row,coulmn,1);
-		       pg.display();
-               if((winner=pg.game_over()) =='X' || winner =='O' )
-		       {	goto end;	}
-		       if(winner=='Z') {               goto gameover;  }
-		       cout<<"\nPlayer-2:";
-		       cin>>row>>coulmn;
-		       pg.take_input(row,coulmn,2);
-		       if((winner=pg.game_over())=='X' || winner =='O')
-		       {	goto end;	}
-		       if(winner=='Z') {               goto gameover;  }
-	        }while(1);
-	        end:
-	        {	cout<<"\nPlayer "<<winner<<" Won";	}
-	        gameover:
-            {        if(winner=='Z'){        cout<<"\nOops....No-one won....!!!";   }         }
-         cout<<"\nDo u want 2 play again..???\nY/N........";
-         cin>>userChoice;
-    }while(toupper(userChoice)!='N');
+	cout << "\nPlayer_1:-->\"X\"\nPlayer_2:-->\"O\"\n";
+	do
+	{
+		do
+		{
+			pg.display();
+			cout << "\nPlayer-1:";
+			cin >> row >> coulmn;
+			pg.take_input(row,coulmn,1);
+			pg.display();
+			if((winner = pg.game_over()) == 'X' || winner == 'O')
+			{
+				goto end;
+			}
+			if(winner == 'Z')
+			{
+				goto gameover;  
+			}
+			cout << "\nPlayer-2:";
+			cin >> row >> coulmn;
+			pg.take_input(row,coulmn,2);
+			if( (winner=pg.game_over()) == 'X' || winner == 'O')
+			{
+				goto end;
+			}
+			if( winner == 'Z' )
+			{
+				goto gameover; 
+			}
+		}while(1);
+
+		end:
+		{
+			cout << "\nPlayer " << winner << " Won";
+		}
+		gameover:
+		{
+			if(winner == 'Z')
+			{
+				cout << "\nOops....No-one won....!!!";
+			}
+		}
+		cout << "\nDo u want 2 play again..???\nY/N........";
+		cin >> userChoice;
+	}while(toupper(userChoice) != 'N');
 }
--- lib/grid.h
+++ lib/grid.h
@@ -8,89 +8,103 @@ using namespace std;
 
 class grid
 {
-	char **p;
-public:
-	void take_input(int, int, int);
-	void display();
-	int check(int, int);
-	char game_over(void);
-	grid();
+	private:
+		char **p;
+	public:
+		void take_input(int, int, int);
+		void display();
+		int check(int, int);
+		char game_over(void);
+		grid(void);
 };
 
-grid :: grid()
+grid :: grid(void)
 {
-	p=new char *[3];
-	for(int i=0;i<3;i++)
-	{	p[i]=new char[3];	}
-	for(int i=0;i<3;i++)
+	p = new char *[3];
+
+	for(int i = 0 ; i < 3 ; i++)
 	{
-		for(int j=0;j<3;j++)
+		p[i]=new char[3];
+		for(int j = 0 ; j < 3 ; j++)
 		{
-			p[i][j]='\0';
+			p[i][j] = '\0';
 		}
 	}
 }
+
 void grid :: take_input(int r, int c, int  pl)
 {
 	start:
-	if((check(r,c))==1)
+	if((check(r,c)) == 1)
 	{
-		if(pl==1)
-		{	p[r-1][c-1]='X';	}
-		else
-		{	p[r-1][c-1]='O';	}
+		if(pl == 1) 
+		{
+			p[r-1][c-1] = 'X';	
+		}
+		else 
+		{	
+			p[r-1][c-1] = 'O';	
+		}
 	}
 	else
 	{
-		cout<<"\nWrong Entry.....!!!\nGive the correct one:";
-		cin>>r>>c;
+		cout << "\nWrong Entry.....!!!\nGive the correct one:";
+		cin >> r >> c;
 		goto start;
 	}
 }
+
 void grid :: display(void)
 {
-	cout<<"\n  | 1 || 2 || 3 |\n";
-	for(int i=0;i<3;i++)
+	cout << "\n  | 1 || 2 || 3 |\n";
+	for(int i = 0 ; i < 3 ; i++)
 	{
-		cout<<i+1<<" ";
-		for(int j=0;j<3;j++)
+		cout << i+1 << " ";
+		for(int j = 0; j < 3 ; j++)
 		{
-			cout<<"| "<<p[i][j]<<" |";
+			cout << "| " << p[i][j] << " |";
 		}
-		cout<<"\n";
+		cout << "\n";
 	}
 }
 
 int grid :: check(int row, int coulmn)
 {
-	if(p[row-1][coulmn-1]=='\0')
-	{	return(1);	}
-	else
-	{	return(0);	}
+	if(p[row-1][coulmn-1] == '\0')
+	{	
+		return 1;	
+	}
+	
+	return 0;
 }
 char grid :: game_over(void)
 {
-	if(p[0][0]==p[0][1] && p[0][0]==p[0][2])  {	return(p[0][0]);	}
-	if(p[0][0]==p[1][0] && p[0][0]==p[2][0])  {	return(p[0][0]);	}
-	if(p[0][0]==p[1][1] && p[0][0]==p[2][2])  {	return(p[0][0]);	}
-	if(p[2][2]==p[2][1] && p[2][2]==p[2][0])  {	return(p[2][2]);	}
-	if(p[2][2]==p[1][2] && p[2][2]==p[0][2])  {	return(p[2][2]);	}
-	if(p[2][0]==p[1][1] && p[1][1]==p[0][2])  {	return(p[1][1]);	}
-	if(p[0][1]==p[1][1] && p[1][1]==p[2][1])  {	return(p[1][1]);	}
-	if(p[1][1]==p[1][0] && p[1][1]==p[1][2])  {	return(p[1][1]);	}
-	int cnt=0;
-    for(int i=0;i<3;i++)
-    {
-            for(int j=0;j<3;j++)
-            {
-                    if(p[i][j]=='\0')
-                    {   cnt++;   }
-            }
-    }
-    if(cnt!=0)
-    {	 return('C');     }
-    else
-    {   return('Z');      }
+	if(p[0][0] == p[0][1] && p[0][0] == p[0][2])  {	return p[0][0];	}
+	if(p[0][0] == p[1][0] && p[0][0] == p[2][0])  {	return p[0][0];	}
+	if(p[0][0] == p[1][1] && p[0][0] == p[2][2])  {	return p[0][0];	}
+	if(p[2][2] == p[2][1] && p[2][2] == p[2][0])  {	return p[2][2];	}
+	if(p[2][2] == p[1][2] && p[2][2] == p[0][2])  {	return p[2][2];	}
+	if(p[2][0] == p[1][1] && p[1][1] == p[0][2])  {	return p[1][1];	}
+	if(p[0][1] == p[1][1] && p[1][1] == p[2][1])  {	return p[1][1];	}
+	if(p[1][1] == p[1][0] && p[1][1] == p[1][2])  {	return p[1][1];	}
+
+	int cnt = 0;
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3 ; j++)
+		{
+			if(p[i][j] == '\0')
+			{
+				cnt++;
+			}
+		}
+	}
+	if(cnt != 0)
+	{
+		return 'C';
+	}
+	
+	return 'Z';
 }
 
 
